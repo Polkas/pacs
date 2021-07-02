f2b07cc1fb0890dbef15f3c377e080c3945c4374
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-07-02 17:59:24 +0200

    exclude_joint arg for true size and R to base arg

--- R/base.R
+++ R/base.R
@@ -64,9 +64,8 @@ pac_deps <- function(pac,
       paks_global,
       c(
         pac,
-        "R",
         if (!base) {
-          base_r
+          c(base_r, "R")
         } else {
           NULL
         }
@@ -235,16 +234,34 @@ pacs_size <- function(pacs = NULL, lib.loc = NULL) {
 #' @param fields character vector, Default: c("Depends", "Imports", "LinkingTo")
 #' @param lib.loc character vector, Default: NULL
 #' @param base logical if to add base packages too. Default: FALSE
+#' @param exclude_joint integer exclude packages which are dependencies of at least N other packages. Default: 0
 #' @return data.frame
 #' @export
 #' @examples
 #' # size in Mb
 #' pacs::pac_true_size("stats")/10**6
-pac_true_size <- function(pac, fields = c("Depends", "Imports", "LinkingTo"), lib.loc = NULL, base = FALSE) {
+#' # exclude packages if at least one other package use it too
+#' \dontrun{
+#' pacs::pac_true_size("devtools", exclude_joint = 1L)/10**6
+#' }
+pac_true_size <- function(pac,
+                          fields = c("Depends", "Imports", "LinkingTo"),
+                          lib.loc = NULL,
+                          base = FALSE,
+                          exclude_joint = 0L) {
   stopifnot(is.null(lib.loc) || all(lib.loc %in% .libPaths()))
   stopifnot(all(fields %in% c("Depends", "Imports", "LinkingTo", "Suggests")))
-  pacs_all <- pac_deps(pac, fields = fields, lib.loc = lib.loc, attr = FALSE, base = base)
-  sum(pacs_size(setdiff(pacs_all$Package, "R"), lib.loc = lib.loc))
+  stopifnot(is.integer(exclude_joint))
+
+  pacs_all <- pac_deps(pac, fields = fields, lib.loc = lib.loc, attr = FALSE, base = base)$Package
+
+  if (exclude_joint) {
+    depsy <- stats::setNames(lapply(pacs_all, function(x) tools::dependsOnPkgs(x)), pacs_all)
+    pacs_all <- setdiff(pacs_all, names(Filter(function(x) length(x) > exclude_joint, depsy)))
+  }
+
+  sum(pacs_size(setdiff(pacs_all, "R"), lib.loc = lib.loc))
+
 }
 
 #' Compare current and expected packages under .libPaths.
